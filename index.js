60c4efac7983d9701bda06dc9b7a00e3f25d3994
rizkysix <rizkysix666@gmail.com>
2024-01-02 00:09:07 +0800

    adding env

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import { config } from "dotenv";
+config({
+  path: '.env'
+})
+
 import { OpenAI } from "langchain/llms/openai";
 import { loadQAMapReduceChain } from "langchain/chains";
 import { Document } from "langchain/document";
@@ -5,8 +10,9 @@ import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { TokenTextSplitter } from "langchain/text_splitter";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
+
 // Optionally limit the number of concurrent requests to the language model.
-const model = new OpenAI({ temperature: 0, maxConcurrency: 10,  openAIApiKey: "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", });
+const model = new OpenAI({ temperature: 0, maxConcurrency: 10,  openAIApiKey: process.env.OPEN_API_TOKEN, });
 const chain = loadQAMapReduceChain(model);
 
 const loader = new PDFLoader("src/Mental.pdf", {
